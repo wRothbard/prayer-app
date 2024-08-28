7e5e539609c909188a033ac2515ee20853c22253
Walter Rothbard <walter.rothbard@gmail.com>
2024-08-28 11:43:48 -0500

    Date selector

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 import Prayer from './Prayer';
 
-const date = new Date();
-
 function App() {
+  const [date, setDate] = useState(new Date());
+  const onChange = (evt) => {
+    setDate(new Date(evt.target.value));
+    console.log(evt.target.value);
+    console.log(date);
+    console.log(date.toISOString().substring(0, 10));
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <input type="date" value={date.toISOString().substring(0, 10)} onChange={onChange} />
         <Prayer dow={ date.getDay() }/>
       </header>
     </div>
