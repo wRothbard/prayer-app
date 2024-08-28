9de32ff895579797995e70b5243d49800c8473cd
Walter Rothbard <walter.rothbard@gmail.com>
2024-08-28 15:53:27 -0500

    Decided to have the date control visible all the time, for now

--- src/App.js
+++ src/App.js
@@ -7,15 +7,11 @@ import { LocalDate } from "@js-joda/core";
 
 function App() {
   const [date, setDate] = useState(LocalDate.now());
-  const [visible, setVisible] = useState(false);
-  const onChangeShow = (evt) => setVisible(evt.target.checked);
   const onChangeDate = (evt) => setDate(LocalDate.parse(evt.target.value));
   return (
     <div className="App">
       <header className="App-header">
-        <input type="checkbox" checked={visible} onChange={onChangeShow} />
-        { visible ? <input type="date" value={date.toString()} onChange={onChangeDate} />
-           : "" }
+        <input type="date" value={date.toString()} onChange={onChangeDate} />
         <Prayer dow={ date.dayOfWeek().value() % 7 }/>
       </header>
     </div>
