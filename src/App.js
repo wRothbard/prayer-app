8bccb9feabdce7ede56db998560f3600edec2d93
Walter Rothbard <walter.rothbard@gmail.com>
2024-08-28 10:35:16 -0500

    Stub prayer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Prayer from './Prayer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Prayer />
       </header>
     </div>
   );
